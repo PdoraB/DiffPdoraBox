d5e429199c08f93be76f50eeeb848adad0e2a0fa
zhaohongqiangsoliva <38648009+zhaohongqiangsoliva@users.noreply.github.com>
2020-06-19 10:13:43 +0800

    add doctoexceling

--- bin/bio_tools/wordtoexcel/doctoexcel.py
+++ bin/bio_tools/wordtoexcel/doctoexcel.py
@@ -16,42 +16,9 @@ from docx import Document
 import pandas as pd
 xlsx= 'test.xlsx'
 xls = pd.ExcelWriter(xlsx)
-word = u'Plus_P2005230074_赵绪明_panel825plus_2020.06.02.docx'
+word = u'P2006090117_李荣国_ct_panel18_2020.06.19-final.docx'
 doc = Document(word)
 tables = doc.tables
-# for i in doc.tables:
-#     print i
-#     print i.cell(0,2).text.lower().strip()
-
-## 查找标题某几个单元格为某几个关键字的表格编号
-# def make_dataframe(f_name, first_cell_string='tag number'):
-#     document = Document(f_name)
-#
-#     # create a list of all of the table object with text of the
-#     # first cell equal to `first_cell_string`
-#     tables = [t for t in document.tables
-#               if t.cell(0,0).text.lower().strip()==first_cell_string]
-#
-#     # in the case that more than one table is found
-#     out = []
-#     for table in tables:
-#         data=[]
-#         for i, row in enumerate(table.rows):
-#             text = (cell.text for cell in row.cells)
-#             if i == 0:
-#                 keys = tuple(text)
-#                 continue
-#
-#             row_data = dict(zip(keys, text))
-#             data.append(row_data)
-#         out.append(pd.DataFrame.from_dict(data))
-#     return out
-# make_dataframe(u"Plus_P2005230074_赵绪明_panel825plus_2020.06.02.docx",u"氨基酸变化")
-
-
-
-
-# print(tables)
 
 for i, tb in enumerate(tables):
     mat = []  # 用 list 套 list 的方法装二维表格内容
@@ -65,9 +32,9 @@ for i, tb in enumerate(tables):
     df = pd.DataFrame(mat,columns=mat[0])
 
     # print df.columns
-    if u"核苷酸\n变化" in df.columns:
-        print df
-        df.to_excel(xls, sheet_name='{i}'.format(**locals()))
+    # if u"核苷酸\n变化" in df.columns:
+    #     print df
+    df.to_excel(xls, sheet_name='{i}'.format(**locals()))
 
 xls.save()  # 保存
 xls.close()  # 关闭
