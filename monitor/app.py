297049cc9cf3ca5897a05b2edfc83200d318cfd1
zhaohongqiangsoliva <38648009+zhaohongqiangsoliva@users.noreply.github.com>
2020-07-02 08:37:28 +0800

    add monitor

--- monitor/app.py
+++ monitor/app.py
@@ -1,12 +1,68 @@
-from flask import Flask
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+@author: soliva
+@Site:
+@file: application.py
+@time: 2020-06-30
+@desc:
+'''
+from flask import Flask, request, render_template, redirect, url_for
+import time, os, re, pandas, json
 
-app = Flask(__name__)
 
+def main(json_file, FILE_DIR):
+    app = Flask(__name__,
+                # template_folder='/gpfs1/RD_project/zhaohongqiang/project/bin/Multi_omics_analysis/Report/',
+                static_url_path='',
+                # static_folder='../',
+                )
+    f = open(json_file, 'r')
+    foundation_dict = json.load(f)
+    if 'personalise' in foundation_dict:
+        pass
+    else:
+        foundation_dict['personalise'] = []
+        print
+        foundation_dict['personalise']
+        with open(json_file, 'w', ) as f:
+            f.write(json.dumps(foundation_dict, encoding='UTF-8', ensure_ascii=False))
+    os.system('ln -sf {} {}'.format(FILE_DIR,
+                                    os.path.split(os.path.realpath(__file__))[0] + '/static', ))
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
+
+        if request.method == 'GET':
+            return render_template('index.html', greeting_list=zip(foundation_dict['personalise'],
+                                                                   range(len(foundation_dict['personalise']) + 1)))
+        else:
+            if 'add' in request.form:
+                project_name = request.form.get('name')
+                png_dir = request.form.get('comment').split('||')
+                data_file = request.form.get('input_text').split('||')
+                project_anno = request.form.get('project')
+                png_anno = request.form.get('png_anno')
+
+                foundation_dict['personalise'].append({"project_name": project_name,
+                                                       'png_dir': png_dir,
+                                                       'data_file': data_file,
+                                                       'project_anno': project_anno,
+                                                       'png_anno': png_anno
+
+                                                       })
+                with open(json_file, 'w', ) as f:
+                    f.write(json.dumps(foundation_dict, encoding='UTF-8', ensure_ascii=False, indent=4))
+                return redirect(url_for('index'))
+            elif 'remove' in request.form:
+                foundation_dict['personalise'].pop()
+                with open(json_file, 'w', ) as f:
+                    f.write(json.dumps(foundation_dict, encoding='UTF-8', ensure_ascii=False, indent=4))
+                return redirect(url_for('index'))
+
+    return app
 
 
 if __name__ == '__main__':
-    app.run()
+    app = main('test')
+    app.run(host='0.0.0.0')
--- monitor/job_monitor.py
+++ monitor/job_monitor.py
@@ -12,17 +12,31 @@ monkey.patch_all()
 import gevent
 import os
 import subprocess
+import pandas as pd
+import sqlite3
+import sys,re
 
-def func(num):
-    print ("start", num)
-    subprocess.call(['sleep', '3'])
-    # sub = subprocess.Popen(['sleep 3'], shell=True)
-    # out, err = sub.communicate()
-    print ("end", num)
+jobs = pd.read_csv("./job.csv",encoding="gbk",header=None,names=["panel","PID","sample_N","sample_T","chip","name"])
+def check_list(df):
+    if df["sample_T"]=="yaml无T":
+        return df["sample_N"]
+    else:
+        return df["sample_T"]
+jobs["check_sample"] = jobs.apply(check_list,axis=1)
 
-g1 = gevent.spawn(func, 1)
-g2 = gevent.spawn(func, 2)
-g3 = gevent.spawn(func, 3)
-g1.join()
-g2.join()
-g3.join()
+def func(sample):
+    print ("start",sample)
+    # subprocess.call("hand ags list|grep -i W067750T",shell=True)
+    sub = subprocess.Popen(f"hand ags list|grep -i {sample}", shell=True)
+    out, err = sub.communicate()
+
+
+
+
+
+job_list=[]
+
+for i in jobs["check_sample"]:
+    job_list.append(gevent.spawn(func, i))
+
+gevent.joinall(job_list)
